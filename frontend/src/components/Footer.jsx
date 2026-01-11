61cbe957cd4acdf0c4b222b50070ad98341fbf47
Thang-Nguyen-Minh <136478740+Thang-Nguyen-Minh@users.noreply.github.com>
2026-01-11 21:53:36 +0700

    feat: add Footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import React from 'react'
+import {cn} from "@/lib/utils.js";
+export const Footer = ({completedTasksCount=0, activeTasksCount=0}) => {
+    return (
+        <>
+            {completedTasksCount + activeTasksCount > 0 && (
+                <div className="text-center">
+                    <p className="text-sm text-muted-foreground">
+                        {
+                            completedTasksCount > 0 &&
+                            <>
+                                🎉Tuyệt vời! Bạn đã hoàn thành {completedTasksCount} việc
+                                {
+                                    activeTasksCount > 0 && `, còn ${activeTasksCount} việc nữa thôi. Cố lên!`
+                                }
+                            </>
+                        }
+                        {completedTasksCount === 0 && activeTasksCount > 0 && (
+                            <>
+                                Hãy bắt đầu làm {activeTasksCount} nhiệm vụ nào!
+                            </>
+                        )}
+                    </p>
+                </div>
+            )}
+        </>
+    )
+}
